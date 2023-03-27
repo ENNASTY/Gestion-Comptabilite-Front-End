8f60c94f822f08abfcf54a34397125fd3dc05b3f
ENNASTY <ennastyhamza42@gmail.com>
2023-03-27 11:44:41 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {DeclarationTvaCreateComponent} from './view/declarationTva/declaration-tva-create/declaration-tva-create.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ToastModule} from "primeng/toast";
+import {ToolbarModule} from "primeng/toolbar";
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import { DeclarationTvaListComponent } from './view/declarationTva/declaration-tva-list/declaration-tva-list.component';
+import {ConfirmationService, MessageService} from "primeng/api";
+import { DeclarationTvaViewComponent } from './view/declarationTva/declaration-tva-view/declaration-tva-view.component';
+import {DialogModule} from "primeng/dialog";
+import {PaginatorModule} from "primeng/paginator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RippleModule} from "primeng/ripple";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeclarationTvaCreateComponent,
+    DeclarationTvaListComponent,
+    DeclarationTvaViewComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        ToastModule,
+        ToolbarModule,
+        FormsModule,
+        ButtonModule,
+        ConfirmDialogModule,
+        DialogModule,
+        PaginatorModule,
+        BrowserAnimationsModule,
+        RippleModule,
+    ],
+  providers: [MessageService, ConfirmationService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/controller/model/declaration-tva-vo2.model.ts
@@ -0,0 +1,8 @@
+import {Facture} from "./facture.model";
+
+export class DeclarationTvaVo2 {
+  public tvacollecter: number;
+  public tvadeductible: number;
+  public differencetva: number;
+
+}
