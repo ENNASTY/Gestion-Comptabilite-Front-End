17dce8866fdc776ba54a6c22b852b073cab00b12
PC <daouiy3@gmail.com>
2023-03-27 13:41:37 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeCreateComponent } from 'src/app/view/employees/employee-create/employee-create.component';
+import { EmployeeListComponent } from 'src/app/view/employees/employee-list/employee-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { FactureCreateComponent } from 'src/app/view/factures/facture-create/facture-create.component';
+import { FactureListComponent } from 'src/app/view/factures/facture-list/facture-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeListComponent,
+    FactureCreateComponent,
+    FactureListComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
