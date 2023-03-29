2657c099c85f2a6ff9479fae20e0c857ee3136ac
ENNASTY <ennastyhamza42@gmail.com>
2023-03-29 13:09:51 +0000

    commit

---
+++ src/app/view/factures/facture-view/facture-view.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {DeclarationTvaService} from "../../../controller/service/declaration-tva.service";
+import {FactureService} from "../../../controller/service/facture.service";
+import {DeclarationTva} from "../../../controller/model/declaration-tva.model";
+import {Facture} from "../../../controller/model/facture.model";
+
+@Component({
+  selector: 'app-facture-view',
+  templateUrl: './facture-view.component.html',
+  styleUrls: ['./facture-view.component.css']
+})
+export class FactureViewComponent {
+  constructor( private service: FactureService) { }
+  public hideViewDialog() {
+    this.viewDialog = false;
+  }
+  get viewDialog(): boolean {
+    return this.service.viewDialog;
+  }
+
+  set viewDialog(value: boolean) {
+    this.service.viewDialog = value;
+  }
+  get facture(): Facture {
+    return this.service.facture;
+  }
+
+  set facture(value: Facture) {
+    this.service.facture = value;
+  }
+
+
+}
